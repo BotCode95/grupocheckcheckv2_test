eaa86124e4b532c94d8a9c15c4468c324a9a60f3
pbottino <pbottino95@gmail.com>
2024-10-30 08:16:52 -0300

    updated menu mobile

--- src/components/navbar/MenuMobile.tsx
+++ src/components/navbar/MenuMobile.tsx
@@ -1,10 +1,13 @@
 import { NavLink } from 'react-router-dom'
 import logo_check_check from '../../assets/logo_checkcheck.svg'
 import { useTranslation } from 'react-i18next'
-import { Grid } from '@mui/material'
+import { Grid, Tooltip } from '@mui/material'
 import { ButtonInscribite } from '../UI/Buttons/ButtonInscribite'
 import { RedSocialList } from '../RedSocial/RedSocialList'
 import { useLanguage } from '../../hooks/useLanguage'
+import { urlWebOtherLanguage } from '../../constants/urls'
+import ukLogo from '../../assets/flags/ukLogo.png'
+
 export const MenuMobile = () => {
 	const [t] = useTranslation('global')
 	const lang = useLanguage()
@@ -20,29 +23,38 @@ export const MenuMobile = () => {
 	}
 
 	return (
-		<div className='navbar_mobile text-white'>
+		<div className="navbar_mobile text-white">
 			<Grid
 				container
-				className='navbar_mobile_container'
+				className="navbar_mobile_container"
 				display={'flex'}
 				justifyContent={'space-between'}
 				alignItems={'center'}
 			>
-				<Grid item sm={6} className='navbar_mobile_logo'>
-					<NavLink
-						aria-current="page"
-						to={`/${lang}/`}
-					>
-						<img src={logo_check_check} alt={t('header.title') || ''} width={70}/>
+				<Grid item sm={3} className="navbar_mobile_logo">
+					<NavLink aria-current="page" to={`/${lang}/`}>
+						<img
+							src={logo_check_check}
+							alt={t('header.title') || ''}
+							width={70}
+						/>
 					</NavLink>
 				</Grid>
-				<Grid item sm={6}
+				<Grid item sm={3} className="navbar_social">
+					<RedSocialList
+						redes={['twitch', 'instagram', 'youtube']}
+						type="contained"
+					/>
+				</Grid>
+				<Grid
+					item
+					sm={2}
 					display={'flex'}
 					justifyContent={'center'}
 					alignItems={'center'}
 					gap={2}
 				>
-					<div className='navbar_mobile_signup'>
+					<div className="navbar_mobile_signup">
 						<NavLink
 							to={`/${lang}/signup`}
 							rel="noreferrer"
@@ -55,16 +67,34 @@ export const MenuMobile = () => {
 						<div className="navbar_mobile_btn_burger"></div>
 					</div>
 				</Grid>
+				<Grid item sm={3}>
+					<Tooltip
+						title={`${
+							lang === 'en' ? 'Ir a Grupo Check Check' : 'Ir a Team Chek Check'
+						}`}
+					>
+						<a
+							href={urlWebOtherLanguage}
+							rel="noreferrer"
+							target="_blank"
+							style={{ textDecoration: 'none' }}
+						>
+							<img src={ukLogo} alt="" width={50} />
+						</a>
+					</Tooltip>
+				</Grid>
 			</Grid>
-			<div className='navbar_mobile_links'>
-				<ul className='d-flex gap-3 list-unstyled m-0 navbar_mobile_links_list'>
+			<div className="navbar_mobile_links">
+				<ul className="d-flex gap-3 list-unstyled m-0 navbar_mobile_links_list">
 					<li>
 						<NavLink
 							to={`/${lang}/teams`}
 							aria-current="page"
-							className='navbar_link'
+							className="navbar_link"
 							style={({ isActive }) => ({
-								color: isActive ? 'var(--secondary-color)' : 'var(--white-color)',
+								color: isActive
+									? 'var(--secondary-color)'
+									: 'var(--white-color)',
 							})}
 						>
 							{t('header.pages.team')}
@@ -74,9 +104,11 @@ export const MenuMobile = () => {
 						<NavLink
 							to={`/${lang}/testimonials`}
 							aria-current="page"
-							className='navbar_link'
+							className="navbar_link"
 							style={({ isActive }) => ({
-								color: isActive ? 'var(--secondary-color)' : 'var(--white-color)',
+								color: isActive
+									? 'var(--secondary-color)'
+									: 'var(--white-color)',
 							})}
 						>
 							{t('header.pages.testimonials')}
@@ -86,9 +118,11 @@ export const MenuMobile = () => {
 						<NavLink
 							to={`/${lang}/faq`}
 							aria-current="page"
-							className='navbar_link'
+							className="navbar_link"
 							style={({ isActive }) => ({
-								color: isActive ? 'var(--secondary-color)' : 'var(--white-color)',
+								color: isActive
+									? 'var(--secondary-color)'
+									: 'var(--white-color)',
 							})}
 						>
 							{t('header.pages.faq')}
@@ -98,17 +132,23 @@ export const MenuMobile = () => {
 						<NavLink
 							to={`/${lang}/blog`}
 							aria-current="page"
-							className='navbar_link'
+							className="navbar_link"
 							style={({ isActive }) => ({
-								color: isActive ? 'var(--secondary-color)' : 'var(--white-color)',
+								color: isActive
+									? 'var(--secondary-color)'
+									: 'var(--white-color)',
 							})}
 						>
 							{t('header.pages.blog')}
 						</NavLink>
 					</li>
 				</ul>
-				<div className='navbar_mobile_social'>
-					<RedSocialList redes={['twitch', 'instagram', 'youtube']} type='contained' width={32} />
+				<div className="navbar_mobile_social">
+					<RedSocialList
+						redes={['twitch', 'instagram', 'youtube']}
+						type="contained"
+						width={32}
+					/>
 				</div>
 			</div>
 		</div>
